84fc6218c27859f498bfa8b0add76ff387f932ec
kun.hk.huang <kun.hk.huang@dniserver.dnish.net>
2024-07-08 16:25:16 +0800

    (feat): [nvim] Introduce LazyNvim as the neovim configuration framework
    Signed-off-by: kun.hk.huang <kun.hk.huang@dniserver.dnish.net>

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      c = { "astyle" },
+    },
+    formatters = {
+      astyle = {
+        command = "astyle",
+        prepend_args = { "-A8", "-s4", "-xC100", "-p", "-k3", "-W3", "-j", "-n" },
+      },
+    },
+  },
+}
